f49337aa30949df22d7516d73c1c10153b89aa1e
Moonjungseob <answjdtjq11@gmail.com>
2024-04-19 12:11:39 +0900

    커밋테스트, hello2 파일 복붙

---
+++ Java_Work/src/ex_240419/Hello2.java
@@ -0,0 +1,17 @@
+package ex_240419;
+
+public class Hello2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Java World");
+		// 규칙1, 메인 함수(메서드)에서 시작하고, 끝난다.
+		// 규칙2, 파일명, 클래스명이 동일 해야한다.
+		// 글자 폰트 크기 단축키 ㅣ ctrl + shitft "+" +또는 "-"
+		// 코딩창 만 보기 : ctrl _ m
+		// 실행 디버깅 없이 : atrl + f11
+		// 클래스 파일 만들고, 오늘 먹은 점심 메뉴 출력해보기.
+	}
+	    // 자동 줄 맞춤
+	    // cntl + shift +f
+
+}
